496917402d353309217f52a237ec6701f80636e9
Proyectos Digitales <tierraadentro@refdgp013.ad.cnca.gob.mx>
2015-03-24 15:55:30 -0600

    Post instalation

---
+++ site/config.php
@@ -0,0 +1,77 @@
+<?php
+
+/**
+ * ProcessWire Configuration File
+ *
+ * Site-specific configuration for ProcessWire
+ *
+ * Please see the file /wire/config.php which contains all configuration options you may
+ * specify here. Simply copy any of the configuration options from that file and paste
+ * them into this file in order to modify them.
+ *
+ * ProcessWire 2.x
+ * Copyright (C) 2014 by Ryan Cramer
+ * Licensed under GNU/GPL v2, see LICENSE.TXT
+ *
+ * http://processwire.com
+ *
+ */
+
+if(!defined("PROCESSWIRE")) die();
+
+/*** SITE CONFIG *************************************************************************/
+
+/**
+ * Enable debug mode?
+ *
+ * Debug mode causes additional info to appear for use during dev and debugging.
+ * This is almost always recommended for sites in development. However, you should
+ * always have this disabled for live/production sites.
+ *
+ * @var bool
+ *
+ */
+$config->debug = false;
+
+
+/*** INSTALLER CONFIG ********************************************************************/
+
+
+/**
+ * Installer: Database Configuration
+ * 
+ */
+$config->dbHost = 'localhost';
+$config->dbName = 'dml2015';
+$config->dbUser = 'root';
+$config->dbPass = 'root';
+$config->dbPort = '8889';
+
+/**
+ * Installer: User Authentication Salt 
+ * 
+ * Must be retained if you migrate your site from one server to another
+ * 
+ */
+$config->userAuthSalt = '0e59d4076707e63f9c1c1b7d10a07366'; 
+
+/**
+ * Installer: File Permission Configuration
+ * 
+ */
+$config->chmodDir = '0755'; // permission for directories created by ProcessWire
+$config->chmodFile = '0644'; // permission for files created by ProcessWire 
+
+/**
+ * Installer: Time zone setting
+ * 
+ */
+$config->timezone = 'America/Mexico_City';
+
+
+/**
+ * Installer: HTTP Hosts Whitelist
+ * 
+ */
+$config->httpHosts = array('localhost:8888', 'localhost', 'publicaciones.conaculta.gob.mx', 'dgp.conaculta.gob.mx');
+
